75ca196d47dfafb2fe374f34a189a283f24fdeef
mang0r <benoit.jaouen@heig-vd.ch>
2024-11-25 15:46:38 +0100

    Utils : variables changed to static

--- labo_smtp/src/main/java/spam/Main.java
+++ labo_smtp/src/main/java/spam/Main.java
@@ -1,20 +1,5 @@
 package spam;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.sql.Time;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
     final static String HOST_NAME = "localhost";
     final static int SERVER_PORT = 1025;
@@ -22,7 +7,7 @@ public class Main {
     final static String END_LINE = "\n";
 
     public static void main(String[] args) {
-        System.out.println(Utils.getVictims());
+        System.out.println("<" + Utils.getVictims() + ">");
         /*try (Socket socket = new Socket(HOST_NAME, SERVER_PORT);
             var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
--- labo_smtp/src/main/java/spam/Utils.java
+++ labo_smtp/src/main/java/spam/Utils.java
@@ -1,22 +1,19 @@
 package spam;
 
 import java.io.File;
-import java.io.FileReader;
-
-import javax.management.ObjectName;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Utils {
-    final String CONFIG_PATH = "../../../../config-files/";
-    final String VICTIMS_CONFIG_FILE_PATH = CONFIG_PATH + "victims.json";
-    final String MESSAGES_CONFIG_FILE_PATH = CONFIG_PATH + "messages.json";
-
-    public static String getVictims () {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(new File(VICTIMS_CONFIG_FILE_PATH));
+    final static String CONFIG_PATH = "../../../../config-files/";
+        final static String VICTIMS_CONFIG_FILE_PATH = CONFIG_PATH + "victims.json";
+        final String MESSAGES_CONFIG_FILE_PATH = CONFIG_PATH + "messages.json";
+    
+        public static String getVictims () {
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(new File(VICTIMS_CONFIG_FILE_PATH));
             String text = jsonNode.asText();
             return text;
 
